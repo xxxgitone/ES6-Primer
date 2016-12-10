3c884ffc58797c60432c8fb9947025efd4700721
xxxgitone <xxxgit@sina.com>
2016-12-10 21:17:42 +0800

    let和const命令

---
+++ src/letAndConst/index.js
@@ -0,0 +1,37 @@
+// {
+//   let a = 10;
+//   var b = 1;
+// }
+
+// a;   //not defined
+// b;
+
+
+// for (let i = 0; i < 10; i++) {}
+
+// console.log(i);//i is not defined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
