7d0a3b692db0e0a5f3bba885f88c772b5d0b29e5
xxxgitone <xxxgit@sina.com>
2017-01-26 15:05:07 +0800

    异步操作和async函数

---
+++ src/async/index.js
@@ -0,0 +1,16 @@
+//Generator函数处理异步
+function* gen() {
+	var url = 'https://api.github.com/users/github';
+	var result = yield fetch(url);
+	console.log(result.bio);
+}
+
+var g = gen();
+var result = g.next();
+
+result.value.then(function (data) {
+	return data.json();
+}).then(function (data) {
+	g.next(data);
+})
+
