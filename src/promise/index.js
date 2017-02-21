74eea09bfa79933960482a084e122a18b987752b
xxxgitone <xxxgit@sina.com>
2017-02-21 21:30:03 +0800

    promise

--- src/promise/index.js
+++ src/promise/index.js
@@ -9,17 +9,42 @@
 // })
 
 //加载图片,一旦加载完成，Promise的状态就发生变化
-const preloadImage = function (path) {
-	return new Promise(function (resolve, reject) {
-		var image = new Image();
-		image.onload = resolve;
-		image.onerror = reject;
-		image.src = path;
+// const preloadImage = function (path) {
+// 	return new Promise(function (resolve, reject) {
+// 		var image = new Image();
+// 		image.onload = resolve;
+// 		image.onerror = reject;
+// 		image.src = path;
+// 	})
+// }
+
+// const timer = new Promise(function (resolve, reject) {
+	
+// 	console.log('Init promise');
+
+// 	setTimeout(function () {
+// 		console.log('Timeout done');
+// 		resolve();
+// 	}, 5000);
+// })
+
+// timer.then(function () {
+// 	console.log('Proceed now that timer has concluded.');
+// })
+
+let timer = function (lenth) {
+	new Promise(function (resolve, reject) {
+		
+		console.log('Init promise');
+
+		setTimeout(function () {
+			console.log('Timeout done');
+			resolve();
+		}, lenth);
 	})
 }
 
-
-
+timer(2000).then(() => console.log('end'))
 
 
 
